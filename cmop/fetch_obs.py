4a8a78e2ffede9acbc8bbd7158781414f36aa867
Jesse Lopez <lopezj@stccmop.org>
2015-07-18 11:05:06 -0700

    Added script to fetch obs from CMOP database.

---
+++ cmop/fetch_obs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""Fetch obs."""
+import datetime
+import data.stationCollection as sc
+
+VARS = ['elev', 'temp', 'salt', 'vel_E', 'vel_N']
+
+
+def fetch_obs(start, end, vars=VARS):
+    """Fetch and save observations from database."""
+    obs = sc.fetchAvailableObservations(start, end, 'obs', variables=vars)
+    obs.saveAsNetCDFCollection()
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('start', type=str, help='Start date (YYYY-MM-DD)')
+    parser.add_argument('end', type=str, help='End date (YYYY-MM-DD)')
+    parser.add_argument('-v', '--var', default=None,
+                        help=('Specify a single variable to fetch '
+                              '(default: elev, salt, temp, vel_E, vel_N)'))
+
+    args = parser.parse_args()
+
+    start = datetime.datetime.strptime(args.start, '%Y-%m-%d')
+    end = datetime.datetime.strptime(args.end, '%Y-%m-%d')
+    if args.var is None:
+        vars = VARS
+    else:
+        vars = args.var
+
+    fetch_obs(start, end, vars)
+
+
+if __name__ == '__main__':
+    main()
